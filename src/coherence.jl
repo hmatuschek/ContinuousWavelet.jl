587ef5789df82e21d83e7ff3c5069ad26693c8c2
Hannes Matuschek <hmatuschek@gmail.com>
2020-02-20 20:36:28 +0100

    Implemented point-wise sig. test for coherence().

--- src/coherence.jl
+++ src/coherence.jl
@@ -4,9 +4,10 @@ struct WaveletCoherence <: AbstractArray{Complex{Float64}, 2}
     coh::Array{Complex{Float64}, 2}
     scales::Array{Float64, 1}
     wavelet::GenericContinuousWavelet
+    α::Float64
 end
 
-function coherence(wt::WaveletTransform, X::Array{Float64, 2})
+function coherence(wt::WaveletTransform, X::Array{Float64, 2}; nsurrogate=0, α=0.05)
     N,M = size(X)
     C = zeros(Complex{Float64}, N, length(wt.scales))
     P = zeros(Float64, N, length(wt.scales))
@@ -17,7 +18,20 @@ function coherence(wt::WaveletTransform, X::Array{Float64, 2})
     end
     C ./= P
 
-    WaveletCoherence(C, wt.scales, wt.wavelet)
+    threshold = undef;
+    if nsurrogate > 0
+        empC = Array{Float64, 1}[]
+        # generate nsurrogate surrogate datasets and obtain coherence
+        for i in 1:nsurrogate
+            Cs = coherence(wt, surrogate(X); nsurrogate=0)
+            append!(empC, reshape(Cs, sum(size(Cs))))
+        end
+        # get threshold corresponding to the given α-level
+        sort!(empC)
+        i = Int((1.0-α)*length(empC))
+        thresholds = empC[(i<1) ? 1 : i]
+    end
+    WaveletCoherence(C, wt.scales, wt.wavelet, threshold)
 end
 
 
@@ -26,6 +40,14 @@ Base.getindex(A::WaveletCoherence, I::Vararg{Int, N}) where {N} = getindex(A.coh
 Base.setindex!(A::WaveletCoherence, v, I::Vararg{Int, N}) where {N} = setindex!(A.coh, v, I...)
 
 function Plots.contourf(A::WaveletCoherence; kw...)
-    contourf(1:size(A.coh)[1], A.scales, transpose(map(abs, A.coh)); kw...);
-    # draw valid range
+    X = 1:size(A.coh)[1]
+    Y = A.scales
+    Z = transpose(map(abs, A.coh))
+    cplt = contourf(X, Y, Z; kw...);
+    # TODO draw valid range
+    # draw point-wise significance level if set
+    if isdefined(A.α)
+        contour!(X, Y, Y; levels=A.α, line_width=3, seriescolor=:reds)
+    end
+    return cplt
 end
