4415b6c726648c4cb2eccc5a3ba0e69b7619aa0a
Hannes Matuschek <hmatuschek@gmail.com>
2020-02-26 14:37:19 +0100

    Added test for cross-coherence.

--- test/coherence.jl
+++ test/coherence.jl
@@ -1,6 +1,6 @@
 using Random: randn
 
-@testset "coherence" begin
+@testset "self-coherence" begin
     N, M = 1024, 128
     t = 1:N
     X = zeros(Float64, N, M)
@@ -8,6 +8,20 @@ using Random: randn
         X[:,i] = sin.(2pi*t/50) + randn(N)
     end
     wt = ContinuousWaveletTransform(CauchyWavelet(10), [50])
-    C = coherence(wt, X)
+    C = coherence(wt, X; nsurrogate=1)
+    @test sum(abs.(C)[:,1])/N ≈ 1.0 atol=1e-1
+end
+
+@testset "cross-coherence" begin
+    N, M = 1024, 128
+    t = 1:N
+    X = zeros(Float64, N, M)
+    Y = zeros(Float64, N, M)
+    for i in 1:M
+        X[:,i] = sin.(2pi*t/50) + randn(N)
+        Y[:,i] = cos.(2pi*t/50) + randn(N)
+    end
+    wt = ContinuousWaveletTransform(CauchyWavelet(10), [50])
+    C = coherence(wt, X, Y, nsurrogate=1)
     @test sum(abs.(C)[:,1])/N ≈ 1.0 atol=1e-1
 end
